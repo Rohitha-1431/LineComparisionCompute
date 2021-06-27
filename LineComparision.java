d9fd3d9c9394b877389d6400a1a36bb7c80f3098
unknown <rohithasatya314@gmail.com>
2021-06-28 00:06:55 +0530

    ADDED:welcome to line comparision program

---
+++ LineComparision.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecomparisioncompute;
+
+public class LineComparision {
+	public static void main(String[] args) {
+		System.out.println("welcome to line cmparision program");		
+	}
+
+}
