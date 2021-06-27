792a083fe4ff3bf2251a170298ce934e226ce22b
unknown <rohithasatya314@gmail.com>
2021-06-28 00:11:07 +0530

    MODIFIED:Length of a Line

--- LineComparision.java
+++ LineComparision.java
@@ -4,5 +4,17 @@ public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("welcome to line cmparision program");		
 	}
-
+	static int x1=10;
+	static int x2=40;
+	static int y1=30;
+	static int y2=40;
+	int length1=0;
+	int length2=0;
+	public void calLengthUC1()
+	{
+		System.out.println("UC1 output:");
+		double length=0;
+		length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1), 2));
+		System.out.println("Length : "+length);
+	}
 }
